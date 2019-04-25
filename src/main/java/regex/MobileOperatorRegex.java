08d3325e20dd9f0915badbebb1330c91261b58af
Oleg <oleg.malyshkin@worldapp.com>
2019-04-25 18:10:20 +0300

    dekete

---
+++ src/main/java/regex/MobileOperatorRegex.java
@@ -0,0 +1,46 @@
+package regex;
+
+import org.apache.log4j.Logger;
+import utils.CountryUtil;
+
+import javax.xml.transform.sax.SAXSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileOperatorRegex {
+
+    private static Logger log = Logger.getLogger( "phoneRegex" );
+
+    public static void getMobileOperatorList(String phoneListText) {
+        HashMap<List<String>, String> mobileOperatorSet = new HashMap();
+        List<String> codeListKyivstar = Arrays.asList( "039", "067", "068", "096", "097", "098" );
+        List<String> codeListLifecell = Arrays.asList( "063", "093" );
+        List<String> codeListMTC = Arrays.asList( "050", "066", "095", "099" );
+        mobileOperatorSet.put( codeListKyivstar, "Kyivstar" );
+        mobileOperatorSet.put( codeListLifecell, "Lifecell" );
+        mobileOperatorSet.put( codeListMTC, "MTC" );
+        String regex = "(?:\\ |W{0,0})(?:(?:((?:\\+?3?8\\s))?\\(?(0\\d\\d))?[^\\w(\\+]{0,2})?(\\d\\d\\W{0,1}\\d\\W{0,1}\\d\\W{0,1}\\d\\W{0,1}\\d\\d)";
+        List<String> phoneList = Arrays.asList( phoneListText.split( "\n" ) );
+        Pattern pattern = Pattern.compile( regex );
+        Matcher matcher = pattern.matcher( phoneList.toString() );
+        for ( String line : phoneList ) {
+            if ( matcher.find() ) {
+                mobileOperatorSet.forEach( ( keyList, value ) -> {
+                    for ( String operatorCode : keyList ) {
+                        if ( operatorCode.equals( matcher.group( 2 ) ) ) {
+                            System.out.println( "Operator " + value + " code " + operatorCode );
+                            log.info( "Operator " + value + " code " + operatorCode  );
+                        }
+                    }
+                } );
+            }
+        }
+    }
+
+
+
+}
--- src/main/java/regex/PhoneNumberRegex.java
+++ src/main/java/regex/PhoneNumberRegex.java
@@ -1,20 +1,21 @@
 package regex;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumberRegex {
     public static void main(String args[]) {
-        //regex: (?:(?:(?:(?:\+?3)?8\W{0,5})?\(?0\W{0,5})?[0-9]\s?\d[^\w,;(\+]{0,5})?\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d(?!(\W?\d))
-        String regex = "(?:(?:(?:(?:\\+?3)?8\\W{0,5})?\\(?0\\W{0,5})?[0-9]\\s?\\d[^\\w,;(\\+]{0,5})?\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d(?!(\\W?\\d))";
-        String string = "0545454545 09-890-234-456 093 9808654 -380915678901 +4 8095 345 6789 093-888 90 80  0918909090 +38 093 2345678 (093)2345678 +38 (098) 345 45 45 +38 (071) 323 45 67";
-        String subst = "";
+        String input = "Oleg Malyshkin 096 43 18 120 \n" +
+                "Oleg Malyshkin (097) 123 45 67\n" +
+                "Oleg Malyshkin +38 (093) 123 45 67\n" +
+                "Oleg Malyshkin 38 (098) 123 45 67\n" +
+                "Oleg Malyshkin 38 095 123 45 67\n" +
+                "Oleg Malyshkin +38 096 123 45 67\n" +
+                "Oleg Malyshkin +38 099 12 34 567\n" +
+                "Oleg Malyshkin +38 (096) 12 34 567";
 
-        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        Matcher matcher = pattern.matcher(string);
-        String result = matcher.replaceAll(subst);
-
-        System.out.println("Substitution result: " + result);
+        MobileOperatorRegex.getMobileOperatorList( input );
 
     }
 }
